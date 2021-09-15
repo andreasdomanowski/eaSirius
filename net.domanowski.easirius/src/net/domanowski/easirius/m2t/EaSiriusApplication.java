fbe6b0cea6cac6dfb57357ea4c9b5cf32fef606b
Andreas Domanowski <andreas@domanowski.net>
2021-09-15 11:10:11 +0200

    feat: add functionality to generate eaSirius editor via a menu entry

--- net.domanowski.easirius/src/net/domanowski/easirius/m2t/EaSiriusApplication.java
+++ net.domanowski.easirius/src/net/domanowski/easirius/m2t/EaSiriusApplication.java
@@ -3,6 +3,12 @@ package net.domanowski.easirius.m2t;
 import java.io.File;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
+import java.util.Objects;
+
+import org.eclipse.core.runtime.ILog;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.emf.ecore.EPackage;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -14,8 +20,25 @@ import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
 import org.eclipse.epsilon.common.util.StringProperties;
 import org.eclipse.epsilon.egl.launch.EgxRunConfiguration;
 import org.eclipse.epsilon.emc.emf.EmfModel;
+import org.eclipse.swt.custom.BusyIndicator;
+import org.eclipse.ui.PlatformUI;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.FrameworkUtil;
+
+import com.crossecore.CrossEcore;
 
 public class EaSiriusApplication {
+	// TODO - replace with SL4J
+	private static final Bundle BUNDLE = FrameworkUtil.getBundle(EaSiriusApplication.class);
+	private static final ILog LOGGER = Platform.getLog(BUNDLE);
+
+	public static void log(String msg) {
+		log(msg, null);
+	}
+
+	public static void log(String msg, Exception e) {
+		LOGGER.log(new Status((e == null ? IStatus.INFO : IStatus.ERROR), BUNDLE.getSymbolicName(), msg, e));
+	}
 
 	private EaSiriusApplication() {
 
@@ -23,18 +46,37 @@ public class EaSiriusApplication {
 
 	public static void generateEditor(String pathToEditorDescriptionString, String pathToMetaModelString,
 			String pathToOutputFolderString) {
+		Objects.requireNonNull(pathToEditorDescriptionString);
+		Objects.requireNonNull(pathToMetaModelString);
+		Objects.requireNonNull(pathToEditorDescriptionString);
+
+		// sanitize output path
+		final String sanitizedOutputFolderString = pathToOutputFolderString.endsWith(File.separator)
+				? pathToOutputFolderString
+				: pathToOutputFolderString + File.separator;
+
+		log("Generating Editor in the following path: " + sanitizedOutputFolderString);
+
 		File editorDescriptionFile = new File(pathToEditorDescriptionString);
 		File metaModelFile = new File(pathToMetaModelString);
 		File targetFolderFile = new File(pathToOutputFolderString);
 
-		executeEcoreTransformations(metaModelFile, targetFolderFile);
+		Runnable generateEditor = () -> {
+			executeEcoreTransformations(metaModelFile, targetFolderFile);
+			log("Generated Properties Editor");
 
-		executeOdesignTransformations(editorDescriptionFile, targetFolderFile);
+			executeOdesignTransformations(editorDescriptionFile, targetFolderFile);
+			log("Generated Editor Application and translated Viewpoint Specification Model");
 
+			String[] crossEcoreArguments = { "-L", "typescript", "-e", pathToMetaModelString, "-p",
+					sanitizedOutputFolderString };
+			CrossEcore.main(crossEcoreArguments);
+		};
+
+		BusyIndicator.showWhile(PlatformUI.getWorkbench().getDisplay(), generateEditor);
 	}
 
-	private static void executeEcoreTransformations(File metaModelFile, File targetFolderFile) {
-
+	private static void executeEcoreTransformations(File metaModelFile, File targetFolderFile) {		
 		Path pathToEgx = Path
 				.of("C:\\Dev\\diplomarbeitWorkspace\\net.domanowski.easirius\\epsilon\\ecoreTransformations.egx");
 
@@ -48,8 +90,8 @@ public class EaSiriusApplication {
 
 		EgxRunConfiguration runConfig = EgxRunConfiguration.Builder().withScript(pathToEgx)
 				.withModel(new EmfModel(), modelProperties)
-				.withParameter("pathPrefix", targetFolderFile.toPath().toAbsolutePath().toString()).withProfiling()
-				.build();
+				.withParameter("pathPrefix", targetFolderFile.toPath().toAbsolutePath().toString() + File.separator)
+				.withProfiling().build();
 
 		runConfig.run();
 	}
@@ -60,25 +102,27 @@ public class EaSiriusApplication {
 
 		StringProperties modelProperties = new StringProperties();
 		modelProperties.setProperty(EmfModel.PROPERTY_NAME, "Model");
+
+		modelProperties.setProperty(EmfModel.PROPERTY_CACHED, "true");
+
+		modelProperties.setProperty(EmfModel.PROPERTY_REUSE_UNMODIFIED_FILE_BASED_METAMODELS, "true");
+
 		modelProperties.setProperty(EmfModel.PROPERTY_METAMODEL_URI,
-				"http://www.eclipse.org/sirius/1.1.0,http://www.eclipse.org/emf/2002/Ecore,http://www.eclipse.org/sirius/diagram/1.1.0");
+				"http://www.eclipse.org/sirius/diagram/1.1.0,http://www.eclipse.org/sirius/1.1.0,http://www.eclipse.org/emf/2002/Ecore");
 		modelProperties.setProperty(EmfModel.PROPERTY_MODEL_URI,
 				editorDescriptionFile.toPath().toAbsolutePath().toUri().toString());
-		modelProperties.setProperty(EmfModel.PROPERTY_READONLOAD, "false");
+		modelProperties.setProperty(EmfModel.PROPERTY_READONLOAD, "true");
 		modelProperties.setProperty(EmfModel.PROPERTY_STOREONDISPOSAL, "true");
 
 		EgxRunConfiguration runConfig = EgxRunConfiguration.Builder().withScript(pathToEgx)
 				.withModel(new EmfModel(), modelProperties)
-				// .withParameter("pathPrefix",
-				// targetFolderFile.toPath().toAbsolutePath().toString()).withProfiling()
-				.build();
+				.withParameter("pathPrefix", targetFolderFile.toPath().toAbsolutePath().toString() + File.separator)
+				.withProfiling().build();
 
 		EPackage.Registry.INSTANCE.entrySet().forEach(c -> {
 			if (c.getKey().equals("http://www.eclipse.org/sirius/1.1.0")) {
-				System.out.println("in here!");
 			}
 		});
-		// System.out.println(modelProperties.get(EmfModel.PROPERTY_METAMODEL_URI));
 		runConfig.run();
 	}
 
@@ -98,11 +142,12 @@ public class EaSiriusApplication {
 		r.getAllContents().forEachRemaining(obj -> {
 			if (obj instanceof EPackage) {
 				EPackage ePackage = (EPackage) obj;
-				System.out.println("Registering " + ePackage.getNsURI());
+				log("Registering " + ePackage.getNsURI());
 				EPackage.Registry.INSTANCE.put(ePackage.getNsURI(), ePackage);
 			}
 		});
 
 		return null;
 	}
+
 }
